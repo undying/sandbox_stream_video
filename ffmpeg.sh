b5842ee42c8b95e171b2a4bd38fea07fbd341f2a
Denis Bozhok <kron@ostrovok.ru>
2020-01-21 22:58:10 +0300

    support directories

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -2,7 +2,7 @@
 
 src=${1:-~/Videos/}
 
-function ffmpeg_flv(){
+function ffmpeg_rtmp(){
   local file="${1}"
 
   ffmpeg \
@@ -26,13 +26,28 @@ function ffmpeg_mpegts(){
     "http://127.0.0.1:8000/publish/"
 }
 
-play_fn=ffmpeg_mpegts
+
+while getopt 'rm' ARG;do
+  case ${ARG} in
+    r)
+      play_fn=ffmpeg_rtmp
+      ;;
+    m)
+      play_fn=ffmpeg_mpegts
+      ;;
+    *)
+      echo "Usage: ${0} -[r|m] stream as rtmp or mpegts (default: mpegts)"
+      exit 1
+  esac
+done
+
+play_fn=${play_fn:-ffmpeg_mpegts}
 
 if [[ -f "${src}" ]];then
   eval ${play_fn} \"${src}\"
 elif [[ -d "${src}" ]];then
-  for i in "${src}"/*{mkv,mp4};do
+  while read -r i;do
     eval ${play_fn} \"${i}\"
-  done
+  done < <(find "${src}" -type f -iname '*.mkv' -o -iname '*.mp4' -o -iname '*.avi')
 fi
 
